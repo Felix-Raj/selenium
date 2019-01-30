6db78ed7aae549bdec4af21ca64f2622b2068460
Felix-Raj <felix.raj@riverbed.com>
2019-01-30 16:42:44 +0530

    change logging format

--- ref/main.py
+++ ref/main.py
@@ -1,10 +1,10 @@
 import logging
 import sys
 
-logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+logging.basicConfig(level=logging.INFO, stream=sys.stdout, datefmt='%d-%b-%y %H:%M:%S')
 logger = logging.getLogger(__name__)
 stdout = logging.StreamHandler(stream=sys.stdout)
-formatter = logging.Formatter('%(asctime)s %(message)s')
+formatter = logging.Formatter('%(asctime)s %(funcName)s %(lineno)d %(message)s')
 stdout.setFormatter(formatter)
 logger.addHandler(stdout)
 logger.setLevel(logging.INFO)
